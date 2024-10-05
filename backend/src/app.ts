bf22448c1ef38a41e2d83d63b6521baa666595df
tv0924@icloud.com <j93es@naver.com>
2024-10-06 01:10:19 +0900

    [Update] index.html lang | use helmet

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,11 @@
 // In a development environment, comment out the next line.
-import "module-alias/register";
+// import "module-alias/register";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 import logger from "morgan";
 import express, { Application, Request } from "express";
-import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
@@ -28,15 +27,16 @@ import {
   requestUtils,
   customLogger,
 } from "@utils/index";
+import helmet from "helmet";
 
 const app: Application = express();
 app.set("trust proxy", "loopback, linklocal, uniquelocal");
 app.set("port", PORT || 8000);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(helmet());
 app.use(cors(corsOptions));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(requestUtils.filterMiddleware);
 
